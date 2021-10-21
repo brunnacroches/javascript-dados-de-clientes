b74e7c1b1f3a40af20353eb5459b44d7cc855dc5
brunnacroches <78552460+brunnacroches@users.noreply.github.com>
2021-10-21 18:37:57 -0300

    Add files via upload

---
+++ 7- DadosClientes.js
@@ -0,0 +1,6 @@
+var cliente = 
+{     id: 40,     nome: "Jorge Mendes",     telefone: "(21) 9999-9999" }; 
+
+console.log(cliente.id);  
+console.log(cliente.nome);  
+console.log(cliente.telefone);
